961613a48f59c5283b0508499588ef0542e6abe8
unknown <JBEAULIE@LZ2626CJBEAULIE.aa.ad.epa.gov>
2016-09-30 16:55:29 -0400

    Working on code to read and merge .dbf files.

--- ohio2016/scriptsAndRmd/readSitesEqAreaData.R
+++ ohio2016/scriptsAndRmd/readSitesEqAreaData.R
@@ -44,4 +44,24 @@ lapply(mylist, ncol)
 colnames(mylist[[14]]) #alum, harsha, tappan, 54 cols
 colnames(mylist[[11]]) #atwood, caesar, harsha, piedmont, 54 cols
 
-colnames(mylist[[6]]) %in% colnames(mylist[[14]])
+colnames(mylist[[5]]) %in% colnames(mylist[[6]])
+
+
+
+# 
+# STRATEGY TO FACILITATE merge
+# Buckhorn, Brookeville, Carr Cr, and Cave Run have 57 columns, compared to 53 or 54 from most other point shapefiles.  Extra columns are due to field included in geodatabase with original polygon shapefile.  
+#                              1.	 Rename GNIS_Name in above = Lake_Name in others
+#                              2.  omit Don’t need: "OBJECTID"   "Permanent_" "FDate"     
+#                              [13] "Resolution" "GNIS_Name"  "Elevation"  "ReachCode"  "FType"     
+#                              [19] "FCode"      
+#                              
+#                              AND
+#                              Most 53 column files contain stuff we don’t need: "Connectivi" "Issue_Type"
+#                              [13] "Lake_Name_" "Reservoir_" "QC"       
+#                              
+#                              2.	 Rename deplyDate to deplyDt
+#                              3.	Add ‘section’ to equal area designs
+#                              4.	Do we need to add ‘stratum’ to Unstratified designs?
+#                              5.	Confirm columns are in same order.
+
