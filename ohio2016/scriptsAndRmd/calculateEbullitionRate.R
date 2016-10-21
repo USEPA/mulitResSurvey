ea409986e5852d4a65b95778b4dc456df2f5c973
unknown <JBEAULIE@LZ2626CJBEAULIE.aa.ad.epa.gov>
2016-10-21 11:12:06 -0400

    minor tweak.

--- ohio2016/scriptsAndRmd/calculateEbullitionRate.R
+++ ohio2016/scriptsAndRmd/calculateEbullitionRate.R
@@ -1,5 +1,12 @@
-eqAreaData <- mutate(eqAreaData, ebMlHrM2 = TtTrpVl / 
-                       (as.numeric(trapRtrvDtTm - trapDeplyDtTm) * ((3.14*.28^2)))) # diameter = 22.25in=0.56m, r=.28m))
+# First calculate volumetric ebullion rate.  Straightforward operation
+# That can be vectorized across the entire df.
+eqAreaData <- mutate(eqAreaData, 
+                     ebMlHrM2 = TtTrpVl / 
+                       (as.numeric(trapRtrvDtTm - trapDeplyDtTm) * 
+                          ((3.14*.28^2)))) # diameter = 22.25in=0.56m, r=.28m))
+
+
+                     ebCh4MgM2h = mass.rate()
 
 
                      
