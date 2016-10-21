4dabf7e485cf648b4fd5e17a8e4a9f991af5b486
Jake Beaulieu <beaulieu.jake@epa.gov>
2016-10-21 12:58:49 -0400

    Working to apply mass.rate function by group.

--- ohio2016/scriptsAndRmd/calculateEbullitionRate.R
+++ ohio2016/scriptsAndRmd/calculateEbullitionRate.R
@@ -5,9 +5,29 @@ eqAreaData <- mutate(eqAreaData,
                        (as.numeric(trapRtrvDtTm - trapDeplyDtTm) * 
                           ((3.14*.28^2)))) # diameter = 22.25in=0.56m, r=.28m))
 
+# Mass flux rate must be calculated by Lake.  Will use group_by in dplyr
 
-                     ebCh4MgM2h = mass.rate()dd
 
 
-                     
+
+
+#32. Custom function with ddply.
+# May need to specify a 'translation' data set in function call.
+#http://stackoverflow.com/questions/20845409/how-do-i-pass-variables-to-a-custom-function-in-ddply
+# d <- data.frame(
+#   experiment = as.factor(c("foo", "foo", "foo", "bar", "bar", "bar")),
+#   si = runif(6),
+#   ti = runif(6)
+# )
+# ddply(d, .(experiment), function(d.sub) cor.test(d.sub$si, d.sub$ti)$statistic)
+# #   experiment         t
+# # 1        bar 0.1517205
+# # 2        foo 0.3387682
+
+
+
+ddply(eqAreaData, .(Lake_Name), function(x) mass.rate(x, choice1 = "ch4"))
+
+
+ #Still trying to figure this out.                    
                      
