205db182e398ac66507680331d3d74befcf06655
William Barnett <wbarnett@Administrators-MacBook-Pro.local>
2018-04-26 21:14:32 -0600

    added relative inf and pd plots

--- ohio2016/scriptsAndRmd/dataAnalysisScripts/runGBM.R
+++ ohio2016/scriptsAndRmd/dataAnalysisScripts/runGBM.R
@@ -1,6 +1,6 @@
 ## Function that takes a response, a set of covariates, and a National/Full
 ## designation - and finds the best parameters from previous simulations
-runGBM <- function(resp, covarType = "Full", seed = 2222){
+runGBM <- function(resp, covarType = "Full", nTrees = 10000, seed = 2222){
   ## This script assumes that the dataGbm object, covarList, and nationalCovar
   ## objects are in the global environment.
   # resp = "co2.trate.mg.h_Estimate"; covarType = "Full"; seed = 2222
@@ -46,5 +46,27 @@ runGBM <- function(resp, covarType = "Full", seed = 2222){
                 bag.fraction = bestPars$bf,
                 cv.folds = 10)
   optTrees <- gbm.perf(tmpGbm)
-  return(list("gbm"=tmpGbm,"optTrees"=optTrees))
+  ## Relative influence plot
+  relInfDf <- tmpGbm %>% summary(plotit = FALSE) %>% filter(rel.inf >= 5)
+  relInfPlot <- ggplot(relInfDf, aes(x = reorder(var, rel.inf), y=rel.inf)) + geom_bar(stat = "identity") +
+    coord_flip() + ylab("Relative Influence (0-100)") + xlab("Variable") +
+    ggtitle(paste("Relative Influence for Response: ", resp, sep=""))
+  ## Partial dependence plots
+  covs <- switch(covarType,
+                 Full = covarList, National = nationalCovar)
+  numPlots <- length(covs)
+  numP1 <- ceiling(numPlots/2)
+  pl <- list()
+  for(i in 1:numPlots){
+    # i = 1
+    p <- partial(tmpGbm, pred.var = covs[i], train = tmpTrain, n.trees = optTrees)
+    f <- plotPartial(p, smooth = TRUE, lwd = 2)
+    pl[[i]] <- f
+  }
+  l1 <- pl[1:numP1]
+  p1 <- gridExtra::marrangeGrob(l1, nrow = 4, ncol = 2)
+  l2 <- pl[(numP1+1):numPlots]
+  p2 <- gridExtra::marrangeGrob(l2, nrow = 4, ncol = 2)
+  return(list("gbm"=tmpGbm,"optTrees"=optTrees,
+              "RelInfPlot"=relInfPlot,"PDPlots"=list(p1,p2)))
 }
